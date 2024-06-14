fc22fa7bd601b23ae2a9fb556232d95221e11117
Yannick Bétemps <arkhee@gmail.com>
2024-06-14 10:36:14 +0200

    Removing debug break

--- src/WebBookScraper.php
+++ src/WebBookScraper.php
@@ -13,22 +13,50 @@ class WebBookScraper
      */
     public $chapters = array();
 
+    private $log = array();
+    private $debug = false;
+    private $logfile = __DIR__.'/log.txt';
+
     /**
      * @param $url
      * @return void
      */
-    public function __construct($url)
+    public function __construct($url,$debug=false)
     {
         $this->url = $url;
+        $this->debug = $debug;
+        if($this->debug)
+        {
+            file_put_contents($this->logfile,"");
+        }
     }
+
+    private function addLog($comment, $url="",$duration=0)
+    {
+        $this->log[]= array(
+            "comment" => $comment,
+            "url" => $url,
+            "duration" => $duration
+        );
+        if($this->debug)
+        {
+            file_put_contents($this->logfile,implode("\t",$this->log).PHP_EOL,FILE_APPEND);
+        }
+    }
+
     public function getBook()
     {
+        $this->addLog("Beginning scraping book");
+        $begin = microtime(true);
         $this->cover = Scraper::Toc($this->url);
-        foreach($this->cover->toc as $toc)
+        $end = microtime(true);
+        $this->addLog("Main page",$this->url,$end-$begin);
+        foreach($this->cover->toc as $index => $toc)
         {
+            $begin = microtime(true);
             $this->chapters[] = Scraper::Chapter($toc->url);
-            // DEBUG
-            //break;
+            $end = microtime(true);
+            $this->addLog("Chapter ".($index+1)." on ".count($this->cover->toc),$toc->url,$end-$begin);
         }
     }
 }
